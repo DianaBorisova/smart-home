fd3ba4152b69e1ca001d4cd562a03018f98ae51a
Diana Borisova <borisova@phystech.edu>
2017-10-13 00:53:32 +0300

    Added sensor

---
+++ src/main/java/ru/sbt/mipt/oop/DoorSensor.java
@@ -0,0 +1,29 @@
+package ru.sbt.mipt.oop;
+
+import ru.sbt.mipt.oop.Door;
+import ru.sbt.mipt.oop.Room;
+import ru.sbt.mipt.oop.SensorEvent;
+import ru.sbt.mipt.oop.SmartHome;
+
+import static ru.sbt.mipt.oop.SensorEventType.DOOR_OPEN;
+
+/**
+ * Created by Диана on 13.10.2017.
+ */
+public class DoorSensor {
+    public static void processDoorEvent(SmartHome smartHome, SensorEvent event) {
+        for (Room room : smartHome.getRooms()) {
+            for (Door door : room.getDoors()) {
+                if (door.getId().equals(event.getObjectId())) {
+                    if (event.getType() == DOOR_OPEN) {
+                        door.setOpen(true);
+                        System.out.println("Door " + door.getId() + " in room " + room.getName() + " was opened.");
+                    } else {
+                        door.setOpen(false);
+                        System.out.println("Door " + door.getId() + " in room " + room.getName() + " was closed.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/sbt/mipt/oop/LightSensor.java
@@ -0,0 +1,45 @@
+package ru.sbt.mipt.oop;
+
+import static ru.sbt.mipt.oop.SensorEventType.DOOR_OPEN;
+import static ru.sbt.mipt.oop.SensorEventType.LIGHT_ON;
+
+/**
+ * Created by Диана on 13.10.2017.
+ */
+public class LightSensor {
+    static void processLightEvent(SmartHome smartHome, SensorEvent event) {
+        for (Room room : smartHome.getRooms()) {
+            for (Light light : room.getLights()) {
+                if (light.getId().equals(event.getObjectId())) {
+                    if (event.getType() == LIGHT_ON) {
+                        light.setOn(true);
+                        System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned on.");
+                    } else {
+                        light.setOn(false);
+                        System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned off.");
+                    }
+                }
+            }
+        }
+    }
+
+    static void turnOffLightsWHenHallDoorIsClosed(SmartHome smartHome, SensorEvent event) {
+        for (Room room : smartHome.getRooms()) {
+            for (Door door : room.getDoors()) {
+                if (door.getId().equals(event.getObjectId())) {
+                    if (event.getType() != DOOR_OPEN) {
+                        if (room.getName().equals("hall")) {
+                            for (Room homeRoom : smartHome.getRooms()) {
+                                for (Light light : homeRoom.getLights()) {
+                                    light.setOn(false);
+                                    SensorCommand command = new SensorCommand(CommandType.LIGHT_OFF, light.getId());
+                                    Application.sendCommand(command);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
